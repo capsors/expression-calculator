a0b6d31ebe2a7b427fdcc637dd7757890911fa84
unknown <capsors22@gmail.com>
2020-03-19 10:51:59 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,83 @@
+
 function eval() {
     // Do not use eval!!!
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  if (expr.match(/\(/) || expr.match(/\)/)) {
+    let leftBr = expr.match(/\(/g);
+    let rightBr = expr.match(/\)/g)
+    if ((leftBr !== null && rightBr !== null) && leftBr.length !== rightBr.length) {
+      throw new Error("ExpressionError: Brackets must be paired");
+    }
+    if ((leftBr === null && rightBr !== null) || (leftBr !== null && rightBr === null)) {
+      throw new Error("ExpressionError: Brackets must be paired");
+    }
+  }
+
+  function calculate(arr) {
+    let count = arr.length;
+    for (let i = 0; i < count; i++) {
+      if (arr.length === 1 || typeof (arr) === 'number') return arr[0];
+
+      if (arr.includes("/")) {
+        let opIndex = arr.findIndex(el => el === "/");
+        if (parseFloat(arr[opIndex + 1]) === 0) throw new Error("TypeError: Division by zero.");
+        arr.splice(opIndex - 1, 3,
+          parseFloat(arr[opIndex - 1]) / parseFloat(arr[opIndex + 1]));
+        continue;
+      }
+      if (arr.includes("*")) {
+        let opIndex = arr.findIndex(el => el === "*");
+        arr.splice(opIndex - 1, 3,
+          parseFloat(arr[opIndex - 1]) * parseFloat(arr[opIndex + 1]));
+        continue;
+      }
+      if (arr.includes("-")) {
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i] === "-") arr[i + 1] *= -1;
+        }
+        let result = arr.filter(el => el !== "-" && el !== "+").map(el => parseFloat(el));
+        arr = result.reduce((sum, el) => sum + el);
+        break;
+      } else {
+        let result = arr.filter(el => el !== "+").map(el => parseFloat(el));
+        arr = result.reduce((sum, el) => sum + el);
+        break;
+      }
+    }
+    return arr;
+  }
+
+  function brackets(arr) {
+    let brArr = [];
+    arr.forEach((el, i) => {
+      if (el === "(") brArr.push(i);
+    });
+
+    let left = brArr.pop();
+    let right = arr.indexOf(")", left);
+    let leftSide = arr.slice(0, left);
+    let rightSide = arr.slice(right + 1);
+    let inBrackets = calculate(arr.slice(left + 1, right));
+    return leftSide.concat(inBrackets, rightSide);
+  }
+
+  let arr = expr.match(/\d+|\+|\-|\*|\/|\(|\)/g);
+  let count = arr.length;
+  for (let i = 0; i < count; i++) {
+    if (arr.length === 1) break;
+
+    if (arr.includes("(")) {
+      arr = brackets(arr);
+      continue;
+    }
+
+    arr = calculate(arr);
+    break;
+  }
+  return parseFloat(arr.toFixed(4));
 }
 
 module.exports = {
